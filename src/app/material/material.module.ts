68e99c40714c0e506a1bf750039164031deaea7c
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-10-19 21:54:31 -0300

    fix: border-color dos inputs resolvido

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+const MaterialComponents = [
+	MatButtonModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatIconModule,
+];
+
+@NgModule({
+	imports: [MaterialComponents],
+	exports: [MaterialComponents],
+})
+export class MaterialModule {}
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -5,11 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MaterialModule } from 'src/app/material/material.module';
 
 import { LoginComponent } from './login.component';
 
@@ -20,10 +16,7 @@ import { LoginComponent } from './login.component';
 		FormsModule,
 		ReactiveFormsModule,
 		FlexLayoutModule,
-		MatFormFieldModule,
-		MatInputModule,
-		MatIconModule,
-		MatButtonModule,
+		MaterialModule,
 	],
 	exports: [LoginComponent],
 })
