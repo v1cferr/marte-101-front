7ff8c6b01978294092dd63bd2d858b930a0637bd
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-10-26 10:15:25 -0300

    feature(recover): email input valid & btn interact

--- src/app/pages/recover-password/recover-password.component.ts
+++ src/app/pages/recover-password/recover-password.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-recover-password',
 	templateUrl: './recover-password.component.html',
 	styleUrls: ['./recover-password.component.scss'],
 })
-export class RecoverPasswordComponent {}
+export class RecoverPasswordComponent {
+	recoverForm = new FormGroup({
+		email: new FormControl('', [Validators.required, Validators.email]),
+	});
+
+	constructor(private router: Router) {}
+	/*
+	 * goToLogin
+	 *
+	 * Rota de retorno para a login page
+	 * (click) on button[class="cancel"]
+	 */
+	goToLogin() {
+		this.router.navigate(['']);
+	}
+}
