d0eaa904689bfb735b991862acc8d53858ebb49e
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-10-30 20:24:39 -0300

    feat(new-pw): requirements validators

--- src/app/pages/new-password/new-password.component.ts
+++ src/app/pages/new-password/new-password.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+	FormControl,
+	FormGroup,
+	Validators,
+	AbstractControl,
+	ValidatorFn,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { WindowService } from 'src/app/services/window.service';
 
@@ -13,22 +19,110 @@ export class NewPasswordComponent {
 
 	constructor(private router: Router, private windowService: WindowService) {}
 
-	public newPasswordForm = new FormGroup({
-		password: new FormControl('', [
-			Validators.required,
-			Validators.minLength(8),
-		]),
-		confirmPassword: new FormControl('', [
-			Validators.required,
-			Validators.minLength(8),
-		]),
-	});
+	/**
+	 * Returns a validator function that checks if the input value contains at least one uppercase letter.
+	 *
+	 * @param {AbstractControl} control - The form control to be validated.
+	 * @return {{ [key: string]: boolean } | null} - Returns an object with the key "uppercase" set to true if the input value does not contain at least one uppercase letter, otherwise returns null.
+	 */
+	public uppercaseValidator(): ValidatorFn {
+		return (control: AbstractControl): { [key: string]: boolean } | null => {
+			if (!/[A-Z]/.test(control.value)) {
+				return { uppercase: true };
+			}
+			return null;
+		};
+	}
 
-	public goToLogin() {
+	/**
+	 * Returns a validator function that checks if the input value contains at least one digit.
+	 *
+	 * @param {AbstractControl} control - The control to be validated.
+	 * @return {{ [key: string]: boolean } | null} - Returns an object with a 'digit' key if the input value does not contain a digit, otherwise returns null.
+	 */
+	public digitValidator(): ValidatorFn {
+		return (control: AbstractControl): { [key: string]: boolean } | null => {
+			if (!/[0-9]/.test(control.value)) {
+				return { digit: true };
+			}
+			return null;
+		};
+	}
+
+	/**
+	 * Returns a validator function that checks if the input value contains a special character.
+	 *
+	 * @return {ValidatorFn} A function that takes an AbstractControl and returns an object with a specialCharacter key if the input value does not contain a special character, or null if it does.
+	 */
+	public specialCharacterValidator(): ValidatorFn {
+		return (control: AbstractControl): { [key: string]: boolean } | null => {
+			if (/[!@#%^&*()_+\-=.,]/.test(control.value)) {
+				return null;
+			}
+			return { specialCharacter: true };
+		};
+	}
+
+	/**
+	 * Returns a validator function that checks if the values of 'newPassword' and 'confirmPassword' match.
+	 *
+	 * @return {ValidatorFn} A validator function that returns an object with a 'passwordMatch' key set to true if the values do not match, or null if they match.
+	 */
+	public passwordMatchValidator(): ValidatorFn {
+		return (control: AbstractControl): { [key: string]: boolean } | null => {
+			const newPassword = control.get('newPassword')?.value;
+			const confirmPassword = control.get('confirmPassword')?.value;
+
+			if (newPassword !== confirmPassword) {
+				return { passwordMatch: true };
+			}
+			return null;
+		};
+	}
+
+	public newPasswordForm = new FormGroup(
+		{
+			newPassword: new FormControl('', [
+				Validators.required,
+				Validators.minLength(8),
+				this.uppercaseValidator(),
+				this.digitValidator(),
+				this.specialCharacterValidator(),
+			]),
+			confirmPassword: new FormControl('', [
+				Validators.required,
+				Validators.minLength(8),
+			]),
+		},
+		{
+			validators: this.passwordMatchValidator(),
+		}
+	);
+
+	/**
+	 * Submits the form and logs the value of the new password form.
+	 *
+	 * @return {void} No return value.
+	 */
+	public submitForm(): void {
+		console.log(this.newPasswordForm.value);
+	}
+
+	/**
+	 * Navigates to the login page.
+	 *
+	 * @return {void} No return value.
+	 */
+	public goToLogin(): void {
 		this.router.navigate(['']);
 	}
 
-	public openSuccessWindow() {
+	/**
+	 * Opens a success window.
+	 *
+	 * @return {void} No return value.
+	 */
+	public openSuccessWindow(): void {
 		this.windowService.openWindow();
 	}
 }
