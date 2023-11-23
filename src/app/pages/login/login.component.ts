f4ea9086f2af18c8b3b19a93b5afdeb86e8cd4c9
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-11-23 18:01:30 -0300

    feat(login): router to home page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,32 @@
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-    public hide: boolean = true;
+	constructor(private router: Router) {}
 
-    public loginForm = new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [
-            Validators.required,
-            Validators.minLength(8),
-        ]),
-    });
+	public hide: boolean = true;
+
+	public loginForm = new FormGroup({
+		email: new FormControl('', [Validators.required, Validators.email]),
+		password: new FormControl('', [
+			Validators.required,
+			Validators.minLength(8),
+		]),
+	});
+
+	/**
+	 * Submits the form and navigates to the home page.
+	 *
+	 * @return {void} - Does not return a value.
+	 */
+	public onSubmit(): void {
+		this.router.navigate(['home']);
+	}
 }
