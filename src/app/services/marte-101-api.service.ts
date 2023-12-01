112b929b5223b3f23c057dddf88dfcc974fbc749
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-12-01 18:09:45 -0300

    Resolve Conflicts

--- src/app/services/marte-101-api.service.ts
+++ src/app/services/marte-101-api.service.ts
@@ -11,6 +11,44 @@ export class Marte101ApiService {
 	constructor(private http: HttpClient) {}
 
 	/**
+	 * Sends a POST request to the user login endpoint.
+	 *
+	 * @param {string} email - The email of the user.
+	 * @param {string} password - The password of the user.
+	 * @param {boolean} rememberMe - Indicates if the user wants to be remembered.
+	 * @return {Promise<any>} A promise that resolves with the response data.
+	 */
+	postUserLogin(
+		email: string,	
+		password: string,
+		rememberMe: boolean
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	): Promise<any> {
+		return this.http
+			.post(`http://localhost:4444/users/login`, {
+				email,
+				password,
+				rememberMe,
+			})
+			.toPromise();
+	}
+
+	/**
+	 * Validates the user token.
+	 *
+	 * @param {string} token - The user token to be validated.
+	 * @return {Promise<any>} A promise that resolves with the validation result.
+	 */
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	postUserTokenValidation(token: string): Promise<any> {
+		return this.http
+			.post<true>(`${this.apiUrl}/users/token-validation`, {
+				token,
+			})
+			.toPromise();
+	}
+
+	/**
 	 * Sends a PATCH request to the server to update the user's password.
 	 *
 	 * @param {string} token - The token for authentication.
