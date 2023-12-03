2ff9b1c9f9878d97a1cb0f54efe44a5b9d5550c8
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-12-03 16:05:32 -0300

    Integracao Cadastro

--- src/app/pages/registration/registration.module.ts
+++ src/app/pages/registration/registration.module.ts
@@ -5,10 +5,17 @@ import { MaterialModule } from 'src/app/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { RegistrationComponent } from './registration.component';
+import { SuccessWindowComponent } from 'src/app/components/success-window/success-window.component';
 
 @NgModule({
 	declarations: [RegistrationComponent],
-	imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+	imports: [
+		CommonModule,
+		MaterialModule,
+		FormsModule,
+		ReactiveFormsModule,
+		SuccessWindowComponent,
+	],
 	exports: [RegistrationComponent],
 })
 export class RegistrationModule {}
