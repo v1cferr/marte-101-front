d00f540df124f90f53449bf78dce287fd7255ebb
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-12-03 14:46:47 -0300

    feat(meteorology): Fahrenheit to Celsius toggle

--- src/app/components/meteorology-card/meteorology-card.component.ts
+++ src/app/components/meteorology-card/meteorology-card.component.ts
@@ -14,4 +14,26 @@ export class MeteorologyCardComponent {
 	@Input() terrestrial_date: string = '';
 	@Input() max_temp: string = '';
 	@Input() min_temp: string = '';
+	@Input() inCelsius: boolean = false;
+
+	/**
+	 * Converts the temperature value based on the current unit setting.
+	 *
+	 * @param {string} value - The temperature value to convert.
+	 * @return {string} - The converted temperature value.
+	 */
+	public convertTemperature(value: string): string {
+		return this.inCelsius ? this.convertToCelsius(value) : value;
+	}
+
+	/**
+	 * Converts a temperature value from Fahrenheit to Celsius.
+	 *
+	 * @param {string} fahrenheit - The temperature value in Fahrenheit.
+	 * @return {string} The converted temperature value in Celsius.
+	 */
+	private convertToCelsius(fahrenheit: string): string {
+		const celsius = ((parseFloat(fahrenheit) - 32) * 5) / 9;
+		return celsius.toFixed(0);
+	}
 }
