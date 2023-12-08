450dbc4e48b191b414920b78b01b8805e2033b2a
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-12-08 18:19:13 -0300

    Guard Genérico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,32 @@ import { NewPasswordComponent } from './pages/new-password/new-password.componen
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MeteorologyComponent } from './pages/meteorology/meteorology.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
 	{ path: '', component: LoginComponent },
 	{ path: 'recover-password', component: RecoverPasswordComponent },
-	{ path: 'new-password', component: NewPasswordComponent },
 	{ path: 'registration', component: RegistrationComponent },
 	{ path: 'home', component: HomeComponent },
-	{ path: 'meteorology', component: MeteorologyComponent },
+	{
+		path: 'new-password/:token',
+		component: NewPasswordComponent,
+	},
+	{
+		path: 'meteorology',
+		component: MeteorologyComponent,
+		canActivate: [AuthGuard],
+	},
+	{
+		path: 'home',
+		component: HomeComponent,
+		canActivate: [AuthGuard],
+	},
+	
 ];
 
 @NgModule({
+	declarations: [],
 	imports: [RouterModule.forRoot(routes)],
 	exports: [RouterModule],
 })
