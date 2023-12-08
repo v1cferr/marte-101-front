5b1b0f443ed02102b99b6329a8640b6eea62fea5
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-12-08 17:42:17 -0300

    feat(about): module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component';
 import { NewPasswordComponent } from './pages/new-password/new-password.component';
-import { RegistrationComponent } from './pages/registration/registration.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MeteorologyComponent } from './pages/meteorology/meteorology.component';
 
 const routes: Routes = [
-	{ path: '', component: LoginComponent },
+	{ path: '', redirectTo: 'about', pathMatch: 'full' },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'registration', component: RegistrationComponent },
 	{ path: 'recover-password', component: RecoverPasswordComponent },
 	{ path: 'new-password', component: NewPasswordComponent },
-	{ path: 'registration', component: RegistrationComponent },
 	{ path: 'home', component: HomeComponent },
 	{ path: 'meteorology', component: MeteorologyComponent },
 ];
