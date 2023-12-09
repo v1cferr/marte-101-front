6f0e0971e06211793c652ea1452c504f6410b872
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-12-09 04:03:32 -0300

    feat(about): tela de sobre

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+	selector: 'app-about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
+	constructor(private router: Router) {}
 
+	/**
+	 * Navigates to the login page.
+	 *
+	 * @return {void} No return value.
+	 */
+	public goToLogin(): void {
+		this.router.navigate(['login']);
+	}
+
+	/**
+	 * Navigates to the registration page.
+	 *
+	 * @return {void} - No return value.
+	 */
+	public goToRegistration(): void {
+		this.router.navigate(['registration']);
+	}
 }
